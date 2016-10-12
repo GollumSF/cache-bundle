3f09d0890dc537659f1814ff635661a85228472e
Damien Duboeuf <smeagolworms4@gmail.com>
2016-10-12 16:00:30 +0200

    Fix APCU

--- Tests/Cache/ApcuCacheTest.php
+++ Tests/Cache/ApcuCacheTest.php
@@ -84,7 +84,7 @@ class ApcuCacheTest extends CacheTestCase {
 	}
 	
 	/**
-	 * @covers GollumSF\CacheBundle\Cache\ApcuCache::has                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           he\ApcuCache::has
+	 * @covers GollumSF\CacheBundle\Cache\ApcuCache::has
 	 * @dataProvider provide
 	 * @depends testSet
 	 */
